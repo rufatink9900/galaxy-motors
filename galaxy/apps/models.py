05acc273070a32925b3f42bb2caca4d204cda1d9
rufatink9900 <rufatink@gmail.com>
2025-12-26 23:10:45 +0400

    edit2

--- galaxy/apps/models.py
+++ galaxy/apps/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 class App(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     apk_file = models.BinaryField(blank=True, null=True)  
     apk_filename = models.CharField(max_length=200, blank=True) 
+    image = CloudinaryField('image', blank=True, null=True)  
+
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
