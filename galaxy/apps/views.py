65d0b91f727f2eee3d06e5debd0d56f8d119c8ac
rufatink9900 <rufatink@gmail.com>
2025-12-27 13:52:12 +0400

    asdasd

--- galaxy/apps/views.py
+++ galaxy/apps/views.py
@@ -1,28 +1,42 @@
 # views.py
 from django.shortcuts import render, get_object_or_404
 from .models import App
+from django.db.models import Q
 from django.http import HttpResponse
 
 
 def apps_list(request):
-    apps = App.objects.all().order_by('-id')
-    return render(request, 'apps_list.html', {'apps': apps})
+    query = request.GET.get('q', '')  # текст поиска
+
+    apps = App.objects.all()
+
+    if query:
+        apps = apps.filter(
+            Q(name__icontains=query) |
+            Q(description__icontains=query)
+        )
+
+    apps = apps.order_by('-id')
+
+    return render(request, 'apps_list.html', {
+        'apps': apps,
+        'query': query
+    })
+
 
 def app_detail(request, pk):
     app = get_object_or_404(App, pk=pk)
     return render(request, 'app_detail.html', {'app': app})
 
 
-
 def download_apk(request, app_id):
     app = get_object_or_404(App, id=app_id)
     if not app.apk_file:
         return HttpResponse("File not found", status=404)
-    
+
     response = HttpResponse(
         app.apk_file,
         content_type='application/vnd.android.package-archive'
     )
     response['Content-Disposition'] = f'attachment; filename="{app.apk_filename}"'
     return response
-
