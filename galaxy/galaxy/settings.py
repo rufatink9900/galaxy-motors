72a04a0e598d2c0a600cbf776c994c481e34d933
rufatink9900 <rufatink@gmail.com>
2025-12-31 14:46:06 +0400

    swsws

--- galaxy/galaxy/settings.py
+++ galaxy/galaxy/settings.py
@@ -145,12 +145,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']  
+STATIC_ROOT = BASE_DIR / 'staticfiles'  
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
